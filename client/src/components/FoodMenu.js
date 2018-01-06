2511f9c995009500597d8a1827d12ffcbff629e9
Nathan Hubner <nathan.w.hubner@gmail.com>
2018-01-06 14:01:53 -0700

    Added Menu Items

--- client/src/components/FoodMenu.js
+++ client/src/components/FoodMenu.js
@@ -1,12 +1,48 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
+import { Header, List } from 'semantic-ui-react';
 
 class FoodMenu extends Component {
   render() {
     return (
       <div>
-        <Header as='h1' textAlign='center'>Menu</Header>
-        <h3 textAlign='center'> Pork </h3>
+        <Header as='h1' textAlign='center'>Pho Tay Ho Menu</Header>
+        <Header as='h3' textAlign='center'> Kids Size 3.75  Small Bowl - 6.50  Large Bowl - 7.50   To-Go - 8.00 </Header>
+        <List>
+          <List.Item as= 'h3'>
+            <List.Icon name='food' />
+            <List.Content>
+              <List.Header as='a'>Tai Nam Gau Gan Sach </List.Header>
+              <List.Description>Steak, Flank, Brisket, Tendon, Tripe</List.Description>
+            </List.Content>
+          </List.Item>
+          <List.Item as= 'h3'>
+            <List.Icon name='food' />
+            <List.Content>
+              <List.Header as='a'>Chin Nam Gau Gan Sach</List.Header>
+              <List.Description>
+                Flank, Brisket, Tendon, Tripe
+              </List.Description>
+            </List.Content>
+          </List.Item>
+          <List.Item as ='h3'>
+            <List.Icon name='food' />
+            <List.Content>
+              <List.Header as='a'>Tai Chin Gau Gan Sach</List.Header>
+              <List.Description>Steak Brisket Tendon Tripe</List.Description>
+            </List.Content>
+          </List.Item>
+          <List.Item as= 'h3'>
+            <List.Icon name='food' />
+            <List.Content>
+              <List.Header as='a'>Tai Nam Gan Sach</List.Header>
+              <List.Description>Steak Flank Tendon Tripe</List.Description>
+            </List.Content>
+          </List.Item>
+  </List>
+
+
+
+
       </div>
     );
   }
